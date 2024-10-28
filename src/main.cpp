bf262744a8b368fdededcb3f8ce31217f6c99da2
alaa2702 <alaa27122002@gmail.com>
2024-10-28 20:47:02 +0300

    app main function is added

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by alaa2 on 10/28/2024.
+//
+#include <stdio.h>
+int main() {
+    unsigned int machineTypeChoice = 0;
+    printf("Finite State Machine App\n");
+
+
+    return 0;
+}
